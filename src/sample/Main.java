12ecd0519b2e04785e34565c89f713d8c6aafde8
Dominik Drag <dominikdrag98@gmail.com>
2020-04-22 20:12:40 +0200

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,16 +14,27 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import sample.model.StockShare;
+import sample.model.Stocks;
 
 
 public class Main extends Application {
     Stage window;
-    private TableView<StockShare> overviewTable =  new TableView<StockShare>();
+    private TableView<StockShare> overviewTable = new TableView<StockShare>();
     final ObservableList<StockShare> listOfStocks = FXCollections.observableArrayList(
-            new StockShare("CDR","HujWieJaki",10 , 10),
-            new StockShare("11bit","HujWieJaki",10 , 10),
-            new StockShare("PKNOrlen","HujWieJaki",10 , 10)
+            new StockShare("CDR", "HujWieJaki", 10, 10),
+            new StockShare("11bit", "HujWieJaki", 10, 10),
+            new StockShare("PKNOrlen", "HujWieJaki", 10, 10)
     );
+
+    private TableView<StockShare> overviewTable = new TableView<StockShare>();
+
+    private Stocks stocks = new Stocks(
+            new StockShare("CDR", "HujWieJaki", 10, 10),
+            new StockShare("11bit", "HujWieJaki", 10, 10),
+            new StockShare("PKNOrlen", "HujWieJaki", 10, 10)
+    );
+
+    final ObservableList<StockShare> listOfStocks = stocks.getStocksList();
 
 
     public static void main(String[] args) {
@@ -31,88 +42,147 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        window = primaryStage;
 
-        Label yourSharesLabel = new Label("Your Stock Shares");
+    public void start(Stage primaryStage) throws Exception {
 
-        overviewTable.setEditable(true);
+        public void start (Stage primaryStage) throws Exception {
 
-        TableColumn<StockShare, String> companyName = new TableColumn<>("Company Name");
-        TableColumn<StockShare, String> stockIndex = new TableColumn<>("Stock Index");
-        TableColumn<StockShare, Double> sharePrice = new TableColumn("Share Price");
-        TableColumn<StockShare, Integer> numberOfShares = new TableColumn("Number of Shares");
-        TableColumn<StockShare, Double> totalValue = new TableColumn("Total Value");
+            window = primaryStage;
 
-        companyName.setCellValueFactory( new PropertyValueFactory<>("companyName"));
-        stockIndex.setCellValueFactory( new PropertyValueFactory<>("stockIndex"));
-        sharePrice.setCellValueFactory( new PropertyValueFactory<>("sharePrice"));
-        numberOfShares.setCellValueFactory( new PropertyValueFactory<>("numberOfShares"));
+            Label yourSharesLabel = new Label("Your Stock Shares");
 
-        companyName.setPrefWidth(116);
-        stockIndex.setPrefWidth(116);
-        sharePrice.setPrefWidth(116);
-        numberOfShares.setPrefWidth(116);
-        totalValue.setPrefWidth(116);
+            overviewTable.setEditable(true);
+
+            TableColumn<StockShare, String> companyName = new TableColumn<>("Company Name");
+            TableColumn<StockShare, String> stockIndex = new TableColumn<>("Stock Index");
+            TableColumn<StockShare, Double> sharePrice = new TableColumn("Share Price");
+            TableColumn<StockShare, Integer> numberOfShares = new TableColumn("Number of Shares");
+            TableColumn<StockShare, Double> totalValue = new TableColumn("Total Value");
 
 
-        overviewTable.setItems(listOfStocks);
-        overviewTable.getColumns().addAll(companyName,stockIndex,sharePrice,numberOfShares,totalValue);
+            companyName.setCellValueFactory(new PropertyValueFactory<>("companyName"));
+            stockIndex.setCellValueFactory(new PropertyValueFactory<>("stockIndex"));
+            sharePrice.setCellValueFactory(new PropertyValueFactory<>("sharePrice"));
+            numberOfShares.setCellValueFactory(new PropertyValueFactory<>("numberOfShares"));
+
+            companyName.setCellValueFactory(new PropertyValueFactory<>("stockName"));
+            stockIndex.setCellValueFactory(new PropertyValueFactory<>("stockIndex"));
+            sharePrice.setCellValueFactory(new PropertyValueFactory<>("sharePrice"));
+            numberOfShares.setCellValueFactory(new PropertyValueFactory<>("numberOfShares"));
 
 
-        VBox tableViewBox = new VBox();
-        tableViewBox.setSpacing(5);
-        tableViewBox.setPadding(new Insets(10,10,10,10));
-        tableViewBox.getChildren().addAll(yourSharesLabel, overviewTable,addNewCompanyToTableView(listOfStocks));
+            companyName.setPrefWidth(116);
+            stockIndex.setPrefWidth(116);
+            sharePrice.setPrefWidth(116);
+            numberOfShares.setPrefWidth(116);
+            totalValue.setPrefWidth(116);
 
 
-        BorderPane overviewLayout = new BorderPane();
-        //overviewLayout.setPadding(new Insets(6,6,6,6));
-        overviewLayout.setCenter(tableViewBox);
-        Scene overviewScene = new Scene(overviewLayout, 600, 400);
+            overviewTable.setItems(listOfStocks);
 
-        window.setTitle("Stocker");
-        window.setScene(overviewScene);
-        window.show();
+            overviewTable.getColumns().addAll(companyName, stockIndex, sharePrice, numberOfShares, totalValue);
 
-    }
+            overviewTable.getColumns().addAll(companyName, stockIndex, sharePrice, numberOfShares, totalValue);
 
-    HBox addNewCompanyToTableView(ObservableList listOfStocks){
-        HBox addNewCompany = new HBox();
 
-        TextField companyName = new TextField();
-        companyName.setPromptText("Company Name");
+            VBox tableViewBox = new VBox();
+            tableViewBox.setSpacing(5);
 
-        TextField stockIndex = new TextField();
-        stockIndex.setPromptText("Stock Index");
+            tableViewBox.setPadding(new Insets(10, 10, 10, 10));
+            tableViewBox.getChildren().addAll(yourSharesLabel, overviewTable, addNewCompanyToTableView(listOfStocks));
 
-        TextField sharePrice = new TextField();
-        sharePrice.setPromptText("Share Price");
+            tableViewBox.setPadding(new Insets(10, 10, 10, 10));
+            tableViewBox.getChildren().addAll(yourSharesLabel, overviewTable, addNewCompanyToTableView(listOfStocks));
 
-        TextField numberOfShares = new TextField();
-        numberOfShares.setPromptText("Number Of Shares");
 
-        Button addButton = new Button("Add Company");
-        addButton.setOnAction(new EventHandler<ActionEvent>() {
-                                  @Override
-                                  public void handle(ActionEvent actionEvent) {
-                                      String sharePriceString;
-                                      String numberOfSharesString;
-                                      listOfStocks.add(new StockShare(
-                                              companyName.getText(),
-                                              stockIndex.getText(),
-                                              10,
-                                              10
-                                      ));
-                                      companyName.clear();
-                                      stockIndex.clear();
-                                      sharePrice.clear();
-                                      numberOfShares.clear();
-                                  }
-                              });
+            BorderPane overviewLayout = new BorderPane();
+            //overviewLayout.setPadding(new Insets(6,6,6,6));
+            overviewLayout.setCenter(tableViewBox);
+            Scene overviewScene = new Scene(overviewLayout, 600, 400);
 
-        addNewCompany.getChildren().addAll(companyName,stockIndex,sharePrice,numberOfShares,addButton);
-        return addNewCompany;
-    }
+            window.setTitle("Stocker");
+            window.setScene(overviewScene);
+            window.show();
 
-}
+        }
+
+
+        HBox addNewCompanyToTableView (ObservableList listOfStocks){
+
+            HBox addNewCompanyToTableView (ObservableList listOfStocks){
+
+                HBox addNewCompany = new HBox();
+
+                TextField companyName = new TextField();
+                companyName.setPromptText("Company Name");
+
+
+                TextField stockIndex = new TextField();
+                stockIndex.setPromptText("Stock Index");
+
+                TextField sharePrice = new TextField();
+                sharePrice.setPromptText("Share Price");
+
+                TextField numberOfShares = new TextField();
+                numberOfShares.setPromptText("Number Of Shares");
+
+                Button addButton = new Button("Add Company");
+                addButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        String sharePriceString;
+                        String numberOfSharesString;
+                        listOfStocks.add(new StockShare(
+                                companyName.getText(),
+                                stockIndex.getText(),
+                                10,
+                                10
+                        ));
+                        companyName.clear();
+                        stockIndex.clear();
+                        sharePrice.clear();
+                        numberOfShares.clear();
+                    }
+                });
+
+                addNewCompany.getChildren().addAll(companyName, stockIndex, sharePrice, numberOfShares, addButton);
+
+
+                TextField stockIndex = new TextField();
+                stockIndex.setPromptText("Stock Index");
+
+                TextField sharePrice = new TextField();
+                sharePrice.setPromptText("Share Price");
+
+                TextField numberOfShares = new TextField();
+                numberOfShares.setPromptText("Number Of Shares");
+
+                Button addCompanyButton = new Button("Add Company");
+                addCompanyButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        String sharePriceString = String.format("%s", sharePrice.getCharacters());
+                        String numberOfSharesString = String.format("%s", numberOfShares.getCharacters());
+                        Double sharePriceDouble = Double.valueOf(sharePriceString);
+                        Integer numberOfSharesInt = Integer.valueOf(numberOfSharesString);
+
+                        stocks.addStock(
+                                new StockShare(
+                                        companyName.getText(),
+                                        stockIndex.getText(),
+                                        sharePriceDouble,
+                                        numberOfSharesInt
+                                ));
+                        companyName.clear();
+                        stockIndex.clear();
+                        sharePrice.clear();
+                        numberOfShares.clear();
+                    }
+                });
+
+                addNewCompany.getChildren().addAll(companyName, stockIndex, sharePrice, numberOfShares, addCompanyButton);
+
+                return addNewCompany;
+            }
+
+        }
