d750d050744bd56119c91d50e76a5b2dd301dc70
Dominik Drag <dominikdrag98@gmail.com>
2020-05-23 16:52:17 +0200

    Move scene setup to MainViewController

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,14 +4,13 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import sample.controller.MainViewController;
+import sample.controller.SideMenuViewController;
 import sample.controller.TableViewController;
 
 
 public class Main extends Application {
-    Stage window;
-    TableViewController tableViewController = new TableViewController();
-    VBox tableViewBox = tableViewController.loadTableView();
-
+    private MainViewController mainViewController = new MainViewController();
 
     public static void main(String[] args) {
         launch(args);
@@ -19,17 +18,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        window = primaryStage;
-
-        BorderPane overviewLayout = new BorderPane();
-        //overviewLayout.setPadding(new Insets(6,6,6,6));
-        overviewLayout.setCenter(tableViewBox);
-        Scene overviewScene = new Scene(overviewLayout, 800, 400);
-
-        window.setTitle("Stocker");
-        window.setScene(overviewScene);
-        window.show();
+        mainViewController.setMainStage(primaryStage);
+        mainViewController.setupMainScene();
 
     }
 
